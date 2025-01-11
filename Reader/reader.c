5ab0731fb788cdcd1e9c84548ca7d445a7550399
FR4TLIZ <150721074+FR4TLIZ@users.noreply.github.com>
2025-01-11 17:17:14 +0100

    Update reader.c

--- Reader/reader.c
+++ Reader/reader.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
 
+int main(void) {
+    FILE *file = fopen("/etc/shadow", "r");
+    if (!file) {
+        perror("No file to read from.");
+        return 1;
+    }
+    char line[256];
+    while (fgets(line, sizeof(line), file)) {
+        printf("%s", line);
+    }
+    fclose(file);
+    return 0;
+}
